79d8c3faf874bab84f9fffb54e5f6019ee06217c
Nicolas <nicolas.silva@utec.edu.uy>
2024-10-21 12:35:16 -0300

    labcontext

--- Lab/Data/LabContext.cs
+++ Lab/Data/LabContext.cs
@@ -9,12 +9,22 @@ namespace Lab.Data
 {
     public class LabContext : DbContext
     {
-        public LabContext (DbContextOptions<LabContext> options)
+        public LabContext(DbContextOptions<LabContext> options)
             : base(options)
         {
         }
 
         public DbSet<Lab.Components.Models.Oficina> Oficina { get; set; } = default!;
         public DbSet<Lab.Components.Models.Tramite> Tramite { get; set; } = default!;
+        public DbSet<Lab.Components.Models.Puesto> Puesto { get; set; } = default!;
+        public DbSet<Lab.Components.Models.Usuario> Usuario { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Oficina>()
+                .HasMany(o => o.ListaPuestos)
+                .WithOne(p => p.Oficina)
+                .HasForeignKey(p => p.idOficina);
+        }
     }
 }
