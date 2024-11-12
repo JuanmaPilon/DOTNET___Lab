29a7b7fbb5fbbdce40754c8f27282b6b12ce4f9e
Nicolas <nicolas.silva@utec.edu.uy>
2024-11-12 19:27:42 -0300

    ahora grafica correctamente en historial los tramites x periodo tiempo

--- Lab/Components/Models/Estadisticas.cs
+++ Lab/Components/Models/Estadisticas.cs
@@ -4,13 +4,11 @@ namespace Lab.Components.Models
 {
     public class Estadisticas
     {
-        [Key]
-        public int idEstadisticas { get; set; }
+        
         public List<Tramite> HistorialTramites {get; set;}
 
-        public void TiempoDemoraProm() { }
-        public void ClientesPorPeriodo(DateTime fechaini, DateTime fechafin) { }
-        public void CantidadDeClientes() { }
+        public void TiempoDemoraProm(int idTramite) { }
+        public void ClientesPorPeriodo(DateTime fechainicio, DateTime fechafin, int idOficina) { }
 
 
 
